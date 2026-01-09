6d46e61e459f04455be257d49ac1d291df2ab1aa
Daniel Gabarro <danielghierro@gmail.com>
2026-01-09 13:31:51 +0100

    Inicio

--- src/test/java/org/example/backendtfggeneral/BackendTfgGeneralApplicationTests.java
+++ src/test/java/org/example/backendtfggeneral/BackendTfgGeneralApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BackendTfgGeneralApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
